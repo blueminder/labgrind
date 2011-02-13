4ac1f493ef0abdb3bac16716656bd36b9b957236
Enrique Santos <me@enriquesantos.net>
2011-02-13 15:00:43 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Labgrind::Application.config.secret_token = '4439b89b6500a3e00df0338d22b9baaf7f3023388c246734344ccfe546c5cefc9e48ff43653f8adc18c580e8c30acb8149b338bcec34c91d25193596f491b609'
