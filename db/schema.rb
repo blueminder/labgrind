0c274bae8745b3a83cdda5c09f0c6977f6cf77ed
Matt Greenland <mpg8389@gmail.com>
2011-02-13 17:32:32 -0500

    Added descriptions to items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110213222631) do
+ActiveRecord::Schema.define(:version => 20110213222838) do
 
   create_table "annotations", :force => true do |t|
     t.string   "text"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(:version => 20110213222631) do
     t.integer  "lab_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "description"
   end
 
   create_table "labs", :force => true do |t|
