d26403acb84d3f48e14d9172f1942893c8581199
jcranmer <Pidgeot18@gmail.com>
2011-04-09 16:34:38 -0400

    Some code documentation.
    I hate git status... it's unreadable...

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -1,4 +1,7 @@
+# The controller for manipulation of annotations.
 class AnnotationsController < ApplicationController
+  # Creates a new annotation for the given item.
+  # After creation, this will redirect to the item page.
   def create
     @item = Item.find(params[:item_id])
     @annotation = @item.annotations.create(params[:annotation])
