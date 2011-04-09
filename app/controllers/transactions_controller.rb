403a17e2310cde8ae290b17156a5cf90ff4b57f6
Matt Greenland <mpg8389@gmail.com>
2011-04-09 19:25:04 -0400

    It'll now attempt to parse entered due dates

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -33,7 +33,8 @@ class TransactionsController < ApplicationController
     if params[:transaction_id] then
       t = Transaction.find(params[:transaction_id])
       if (params[:transaction][:due_date]) then
-        t.due_date = params[:transaction][:due_date]
+        # TODO: Handle date parsing, invalid dates, etc. better.
+        t.due_date = DateTime.parse params[:transaction][:due_date]
       end
       t.status = "Complete"
       t.save
