cdcfd1749e112239172adb1c4d4694a36ccdb894
Matt Greenland <mpg8389@gmail.com>
2011-04-16 16:41:40 -0400

    In retrospect, I probably should have just generated a scaffold in the first place.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,7 @@
+# A calendar event.
+# This simply respresents some kind of event that people want to make note
+# of, that runs for a specific period of time. It can be marked as exclusive,
+# meaning the lab can be reserved for that period of time, and (TODO) it can
+# also be tied to a specific project.
 class Event < ActiveRecord::Base
 end
