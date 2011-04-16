90837c5b18fd51a5d79f98a2453254fd529dc83d
Matt Greenland <mpg8389@gmail.com>
2011-04-16 17:11:35 -0400

    Events can now check if they conflict with any other events

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,17 @@
 # also be tied to a specific project.
 class Event < ActiveRecord::Base
   belongs_to :lab
+
+  # Checks to see if, at any point, this event is running concurrently with
+  # another event.
+  def overlaps? other
+    not (end_time < other.start_time or start_time > other.end_time)
+  end
+
+  # Checks to see if this event conflicts with another event.
+  # Two events conflict if they run at the same time, in the same lab, and if
+  # at least one of them is exclusive.
+  def conflicts_with? other
+    lab == other.lab and overlaps? other and (exclusive or other.exclusive)
+  end
 end
