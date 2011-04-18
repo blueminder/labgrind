f8ff9be7b385c74da015efe19ea01e4bef2f8af5
Enrique Santos <me@enriquesantos.net>
2011-04-18 02:26:50 -0400

    previous missing files

---
+++ app/models/project.rb
@@ -0,0 +1,11 @@
+class Project < ActiveRecord::Base
+  has_many :project_assignments
+  has_many :users, :through => :project_assignments
+  
+  def set_owner(user)
+    assignment = ProjectAssignment.find(:first, :conditions => { :project_id => self.id, :user_id => user.id })
+    assignment.owner = 1
+    assignment.save
+  end
+  
+end
