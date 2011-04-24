7d78636dd4485a7043ea8c1a5e49395843fc62c9
Enrique Santos <me@enriquesantos.net>
2011-04-24 16:59:19 -0400

    upload multiple images with captions (with more jQuery goodness, it's still a glorious novelty to me right now.)

--- app/models/project_image.rb
+++ app/models/project_image.rb
@@ -1,6 +1,6 @@
 class ProjectImage < ActiveRecord::Base
   belongs_to :project
-  has_attached_file :image, :styles => { :large => "120x120>", :medium => "48x48>", :thumb => "26x26>" }
+  has_attached_file :image, :styles => { :full => "640x480>", :large => "120x120>", :large_thumb => "100x100#" }
   validates_attachment_presence :image
   validates_attachment_size :image, :less_than => 5.megabytes
 end
