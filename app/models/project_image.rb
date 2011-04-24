26974e501b55241b6ce484221e5b2946dd534dc5
jcranmer <Pidgeot18@gmail.com>
2011-04-24 15:40:03 -0400

    What have I said before about updating documentation? Back up to 100.00% again.

--- app/models/project_image.rb
+++ app/models/project_image.rb
@@ -1,3 +1,4 @@
+# An object that represents the image of a project
 class ProjectImage < ActiveRecord::Base
   belongs_to :project
   has_attached_file :image, :styles => { :large => "120x120>", :medium => "48x48>", :thumb => "26x26>" }
